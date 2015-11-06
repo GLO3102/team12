de7f7744406034ecb5936225166ee0eaa68b06a8
Alexandre <Alexandre937@hotmail.com>
2015-11-05 22:50:03 -0500

    Watchlist - Modification avec colllection

--- umovie-backbone/js/views/watchlist.js
+++ umovie-backbone/js/views/watchlist.js
@@ -11,15 +11,24 @@ $(function() {
 });
 
 $(function() {
+    WatchlistCollection = Backbone.Collection.extend({
+        model: WatchlistModel,
+        parse: function(response) {
+            return response.movies;
+        }
+    });
+});
+
+$(function() {
     WatchlistView = Backbone.View.extend({
         template: _.template($("#watchlist-view").html()),
         el: ".page",
 
         initialize: function() {
-            this.render();
+			this.render();
         },
         render: function() {
-            this.$el.html(this.template({
+			this.$el.html(this.template({
                 watchlist: this.model.toJSON()
             }));
         }
