1c761fe52ba0c898e1e2932c7dc93b427b454482
Philippe Desmarais <UltraSatellite@gmail.com>
2015-11-07 15:15:06 -0500

    Added Youtube Trailers for TV series

--- umovie/js/views/TVShow.js
+++ umovie/js/views/TVShow.js
@@ -35,10 +35,27 @@ var app = app || {};
                 this.render();
             },
             render: function () {
+                var modelJSON = this.model.toJSON();
+                var youtubeVideo = this.findYoutubeVideo(modelJSON.artistName + 'trailer');
                 this.$el.html(this.template({
-                    mod: this.model.toJSON(),
-                    episodes: this.collection.toJSON()
+                    mod: modelJSON,
+                    episodes: this.collection.toJSON(),
+                    youtubeVideoId: youtubeVideo
                 }));
+            },
+            findYoutubeVideo: function(keyword) {
+                var url = 'https://www.googleapis.com/youtube/v3/search?part=id&q=' + keyword + '&type=video&key=AIzaSyA2Sh1KhOUfKYpfQchT5oZPgO0PpTqB17M';
+                var videos = undefined;
+                $.ajaxSetup({
+                    async: false
+                });
+                $.getJSON(url, function(data) {
+                    videos = data;
+                });
+                $.ajaxSetup({
+                    async: true
+                });
+                return videos.items[0].id.videoId;
             }
 
         });
